2b6a89c4d2fd78340caf7501cca04c3bdddb43c4
joseporras00 <55441394+joseporras00@users.noreply.github.com>
2021-03-11 02:00:32 +0100

    Add files via upload

---
+++ HillClimbing.py
@@ -0,0 +1,134 @@
+import random
+import math
+
+def evaluarSolucion(datos, solucion):
+    longitud = 0
+    for i in range(len(solucion)):
+        longitud += datos[solucion[i - 1]][solucion[i]]
+    return longitud
+
+def obtenerMejorVecino(solucion, datos):
+    ##Obtención de los vecinos
+    vecinos = []
+    l=len(solucion)
+    for i in range(l):
+        for j in range(i+1, l):
+            n = solucion.copy()
+            n[i] = solucion[j]
+            n[j] = solucion[i]
+            vecinos.append(n)
+
+    ##Obtención del mejor vecino
+    mejorVecino = vecinos[0]
+    mejorLongitud = evaluarSolucion(datos, mejorVecino)
+    for vecino in vecinos:
+        longitud = evaluarSolucion(datos, vecino)
+        if longitud < mejorLongitud:
+            mejorLongitud = longitud
+            mejorVecino = vecino
+    return mejorVecino, mejorLongitud
+
+def hillClimbing(datos):
+    l=len(datos)
+    ##Creamos una solucion aleatoria
+    ciudades = list(range(l))
+    solucion = []
+    for i in range(l):
+        ciudad = ciudades[random.randint(0, len(ciudades) - 1)]
+        solucion.append(ciudad)
+        ciudades.remove(ciudad)
+    longitud = evaluarSolucion(datos, solucion)
+
+    print("Longitud de la ruta: ", longitud)
+    ##Obtenemos el mejor vecino hasta que no haya vecinos mejores
+    vecino = obtenerMejorVecino(solucion, datos)
+    while vecino[1] < longitud:
+        solucion = vecino[0]
+        longitud = vecino[1]
+        print("Longitud de la ruta: ", longitud)
+        vecino = obtenerMejorVecino(solucion, datos)
+
+    return solucion, longitud
+
+def iterated(datos):
+    l=len(datos)
+    minLong=99999999
+    ciudades=list(range(l))
+    solucion=[]
+    for i in range(1):
+        x=random.randint(0,len(ciudades) - 1)
+        ciudad=ciudades[x]
+        ciudades.remove(ciudad)
+    longitud=evaluarSolucion(datos,solucion)
+
+    vecino=obtenerMejorVecino(solucion,datos)
+    contador=1
+    while vecino[1] < longitud:
+        solucion=vecino[0]
+        longitud=vecino[1]
+        vecino=obtenerMejorVecino(solucion,datos)
+        contador=contador+1
+
+    while contador + 2< (len(datos)-1):
+        aux=contador
+        aux1=0
+        newSolucion=[]
+
+        for i in range(1):
+            if aux <(len(datos)-1):
+                newSolucion.append(solucion[aux])
+                aux=aux+1
+            else:
+                newSolucion.append(solucion[aux1])
+                aux1=aux1+1
+
+        auxLong=evaluarSolucion(datos,newSolucion)
+
+        if auxLong < minLong:
+            auxVecino=obtenerMejorVecino(newSolucion,datos)
+            contaodr=contaodr+2
+            while auxVecino[1]<auxLong:
+                newSolucion=auxVecino[0]
+                auxLong=auxVecino[1]
+                auxVecino=obtenerMejorVecino(newSolucion,datos)
+                contaodr=contador+1
+            minLong=auxLong
+
+    if minLong < longitud:
+        return newSolucion, auxLongitud
+    else:
+        return solucion.longitud
+
+
+def mejorado(datos):
+    iteraciones=20
+    result=[]
+    for i in range(1,10):
+        distancia=[]
+        bestDist=math.inf
+        worstDist=0
+        sumDist=0
+        for j in range(iteraciones):
+            s=iterated(datos)
+            distancia.append(s[1])
+            sumDist+=s[1]
+            if s[1] < bestDist:
+                bestDist=s[1]
+            elif s[1] > worstDist:
+                worstDist=s[1]
+
+        optimalOcurr=distances.count(bestDist)
+        results.append([i,bestDist,worstDist,sumDist/iteraciones,optimalOcurr/iteraciones])
+        for res in results:
+            print(",".join([str(s) for s in res]) + "\n")
+
+def main(datos):
+    
+
+    s=hillClimbing(datos)
+    print("--------------")
+    print("Solucion final: ",s[0])
+    print("Longitud de la ruta final: ",s[1])
+
+if __name__ == "__main__":
+    main()
