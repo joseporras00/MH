0001a28c771ac8ce777b7fa9219361e2190d5d4f
joseporras00 <55441394+joseporras00@users.noreply.github.com>
2021-04-27 18:41:31 +0200

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -1,3 +1,11 @@
+def linea_mas_larga(datos):
+    lmaslarga = datos[0]
+    for linea in datos:
+        if len(linea) > len(lmaslarga):
+            lmaslarga = linea
+    if len(lmaslarga)>=4:
+        return lmaslarga
+
 def main():
     elite=[]
     datos=[]
@@ -7,6 +15,9 @@ def main():
         for linea in lineas:
             datos.append(linea.split(sep=":"))
     print(datos[2])
+
+    grafo1=linea_mas_larga(datos)
+    print(grafo1)
 
     
 
